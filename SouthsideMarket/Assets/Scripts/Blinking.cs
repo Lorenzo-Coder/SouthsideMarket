84d0c3cfefefe2877a503784edaa2f9f0c0b1b70
jlu787 <jlu787@outlook.com>
2019-08-05 20:10:26 +1200

    Made groceries blink before falling

---
+++ SouthsideMarket/Assets/Scripts/Blinking.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinking : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    public Rigidbody2D rb;
+    public float blinkDuration = 2.0f;
+    public float blinkInterval = 0.5f;
+
+    private float durationTimer = 0.0f;
+    private float intervalTimer = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        durationTimer += Time.deltaTime;
+        intervalTimer += Time.deltaTime;
+
+        if (intervalTimer >= blinkInterval)
+        {
+            sprite.enabled = !sprite.enabled; // set to opposite
+            intervalTimer = 0.0f; // reset timer
+        }
+
+        if (durationTimer > blinkDuration)
+        {
+            rb.gravityScale = 1;
+            sprite.enabled = true;
+        }
+    }
+}
